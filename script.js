f062f46c9408f24c63c45f87ff19473328ca1f1b
Augusta Christensen <augusta@castagna.dk>
2023-01-05 22:43:12 +0100

    projects side - inden ændringer

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ document.querySelector("#burger-c").addEventListener("click", burgemenu);
 function burgemenu() {
   document.querySelector(".mobil-menu").classList.toggle("menu-v");
 }
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
