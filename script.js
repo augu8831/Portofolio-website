a4b8f3dc0f90eb3cd27e6b86563b87105f9ce7b9
Augusta Christensen <augusta@castagna.dk>
2023-01-06 13:13:27 +0100

    meget justering på alle sider

--- script.js
+++ script.js
@@ -1,23 +1,70 @@
-document.querySelector("#burger-c").addEventListener("click", burgemenu);
+(() => {
+  //console.clear();
+  const nav = document.querySelector("nav:where(header *)");
+  if (!nav) return;
+  const breakpoint = window.getComputedStyle(nav).getPropertyValue("--less-than") || "500";
+  const mobileMQ = window.matchMedia(`(max-width: ${breakpoint - 1}px)`);
+  const iconClose = "M18 6L6 18M6 6l12 12";
+  const iconMenu = "M4 12h16M4 6h16M4 18h16";
+  const svg = (d) => `<svg viewBox="0 0 24 24" width="24" height="24">
+      <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="${d}"></path>
+    </svg>`;
 
-function burgemenu() {
-  document.querySelector(".mobil-menu").classList.toggle("menu-v");
-}
+  const btn = document.createElement("button");
+  btn.setAttribute("aria-expanded", false);
+  btn.setAttribute("aria-label", "Vis menu");
+  btn.innerHTML = svg(iconMenu);
+  nav.prepend(btn);
 
-function reveal() {
-  var reveals = document.querySelectorAll(".reveal");
+  btn.addEventListener("click", clickHandler);
 
-  for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
+  function clickHandler() {
+    let expanded = this.getAttribute("aria-expanded") === "true" || false;
 
-    if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
-    }
+    this.setAttribute("aria-expanded", !expanded);
+    this.setAttribute("aria-label", !expanded ? "Skjul menu" : "Vis menu");
+
+    this.innerHTML = svg(!expanded ? iconClose : iconMenu);
   }
-}
 
-window.addEventListener("scroll", reveal);
+  const handleMobileChange = ({ matches }) => {
+    btn.hidden = matches ? false : true;
+    nav.dataset.menuBtnShown = matches ? true : false;
+  };
+
+  mobileMQ.addListener(handleMobileChange);
+
+  handleMobileChange(mobileMQ);
+
+  const injectCSS = (css) => {
+    const style = document.createElement("style");
+    style.innerText = css;
+    document.head.appendChild(style);
+    return style;
+  };
+  injectCSS(`\
+      [hidden] {display: none !important;}\
+      [aria-expanded="false"]:not([hidden]) + ul { display: none; }\
+      header { flex-wrap: wrap; }\
+      nav[data-menu-btn-shown="true"] { display: contents; }\
+      nav:only-child, nav:only-child button { margin-inline-start: auto; }\
+      [aria-expanded="true"]:not([hidden]) + ul { flex-flow: column; flex-basis: 100%; }\
+      button[aria-expanded] {\
+        background: unset;\
+        border: 2px solid var(--burger-border, #e2e1e4);\
+        box-sizing: border-box;\
+        border-radius: 6px;\
+        inline-size: 2.25rem;\
+        block-size: 2.25rem;\
+        padding: .25rem;\
+        cursor: pointer;\
+        color: var(--burger-lines, currentColor);\
+      }\
+      svg {\
+        display: block;\
+        inline-size: 100%;\
+        block-size: 100%;\
+        color: currentColor;
+      }\
+    `);
+})();
